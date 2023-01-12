c00ddb16b1b8cf01c08408ca22d662a3ebcbc76b
acer <rita.bapyeva55@gmail.com>
2023-01-12 15:53:52 +0300

    comment 1

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+    private  String name;
+    private String surname;
+    private String email;
+    private  String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public  void setAge(int jach){
+        if (jach>60){
+
+        }
+
+}
+}
